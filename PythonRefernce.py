6f384f8eee362281cfd0ecd20d28c93b12e8a27f
Steve Owens <stevejowens@icloud.com>
2019-03-14 20:46:48 +0000

    Created the Python reference file.

---
+++ PythonRefernce.py
@@ -0,0 +1,33 @@
+# Python Learning Reference
+"""This document is a guides on all of the Pyhton syntax and language
+Please use at your own risk"""
+
+#Get install version of Python
+pyhton --version
+
+# Run a Python file from the command line
+pyhton {filename}.py
+
+#Access the Pyhton command Line
+pyhton
+
+#Exit the Pyhton command line
+exit()
+
+#Variables
+x = 5 # X is an int
+Y = "John" # y is a string
+_x = 1 # A variable must start with a letter or an underscore
+x_1 = "Steve" # A variable can only container alphanumeric characters and underscores
+X_2 = 2 # Variable names are case sensitive
+
+# Numbers
+"""Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length. """
+x = 1
+x = -1
+
+"""Float, or "floating point number" is a number, positive or negative, containing one or more decimals."""
+x = 1.0
+x = 1.12
+x = -35.243
+

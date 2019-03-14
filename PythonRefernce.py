e40a1f9409f44ca151b036c2cac629a4231ecf9b
Steve Owens <stevejowens@icloud.com>
2019-03-14 22:37:10 +0000

    Add collections (array) to python reference file

--- PythonRefernce.py
+++ PythonRefernce.py
@@ -158,4 +158,108 @@ exit()
     x >>= 3	#Same as	x = x >> 3	
     x <<= 3	#Same as	x = x << 3
 
+#Collections (Arrays)
+    """List - A list is a collection which is ordered and changeable. In Python lists are written with square brackets."""
+    thislist = ["apple", "banana", "cherry"]
+    print(thislist)
+
+    """Access items - You access the list items by referring to the index number"""
+    thislist = ["apple", "banana", "cherry"]
+    print(thislist[1]) # Prints the second item in the list
+
+    """Change Item Value - To change the value of a specific item, refer to the index number"""
+    thislist = ["apple", "banana", "cherry"]
+    thislist[1] = "orange"
+    print(thislist)
+
+    """Loop through a list - You can loop through the list items by using a for loop"""
+    thislist = ["a", "b", "c"]
+    for x in thislist:
+        print(x)
     
+    """Check if item exists - To determine if a specified item is present in a list use the in keyword"""
+    thislist = ["apple", "banana", "cherry"]
+    if "apple" in thislist:
+        print("Yes, 'apple' is in the fruits list")
+
+    """List Length - To determine how many items a list has, use the len() method"""
+    thislist = ["apple", "banana", "cherry"]
+    print(len(thislist))
+
+    """Add Items"""
+        """To add an item to the end of the list, use the append() method"""
+        thislist = ["apple", "banana", "cherry"]
+        thislist.append("orange")
+        print(thislist)
+
+        """To add an item at the specified index, use the insert() method"""
+        thislist = ["apple", "banana", "cherry"]
+        thislist.insert(1, "orange")
+        print(thislist)
+    
+    """Remove Item - There are several methods to remove items from a list"""
+        """The remove() method removes the specified item"""
+        thislist = ["apple", "banana", "cherry"]
+        thislist.remove("banana")
+        print(thislist)
+
+        """The pop() method removes the specified index, (or the last item if index is not specified)"""
+        thislist = ["apple", "banana", "cherry", "orange", "lemon", "lime"]
+        thislist.pop()
+        thislist.pop(1)
+        print(thislist)
+
+        """The del keyword removes the specified index"""
+        thislist = ["apple", "banana", "cherry"]
+        del thislist[0]
+        print(thislist)
+
+        """The del keyword can also delete the list completely"""
+        thislist = ["apple", "banana", "cherry"]
+        del thislist
+
+        """The clear() method empties the list"""
+        thislist = ["apple", "banana", "cherry"]
+        thislist.clear()
+        print(thislist)
+
+    #list() constructor - It is also possible to use the list() constructor to make a list
+    thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+    print(thislist)
+
+    #List methods
+    append() #Adds an element at the end of the list
+    clear()	#Removes all the elements from the list
+    copy()	#Returns a copy of the list
+    count()	#Returns the number of elements with the specified value
+    extend() #Add the elements of a list (or any iterable), to the end of the current list
+    index()	#Returns the index of the first element with the specified value
+    insert() #Adds an element at the specified position
+    pop() #Removes the element at the specified position
+    remove() #Removes the item with the specified value
+    reverse() #Reverses the order of the list
+    sort() #Sorts the list
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
